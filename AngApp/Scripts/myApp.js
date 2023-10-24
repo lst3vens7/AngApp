905e72ff27cb22617f4dc8262121f4ba432edd44
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-10-24 10:11:00 +0100

    Add project files.

---
+++ AngApp/Scripts/myApp.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('MyApp', []);
+app.controller('MyController', function ($scope) {
+    $scope.MyVal = "Welcome to Angular.js";
+
+    $scope.MyFun = function () {
+        alert('Hello World!')
+    }
+    $scope.MyFun2 = function (val) {
+        alert('Parameter passed: ' + val);
+    }
+});
